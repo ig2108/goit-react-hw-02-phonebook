d2542266e8d3619bcb6113d9a09ad427f651155e
Ivan Golubey <ig2108@yahoo.com>
2020-04-02 19:32:35 +0300

    complete hw-02 task 2

---
+++ build/precache-manifest.c6924ef58ec950b4688f4d3cc2d4e84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1239009d4855e705edff31dd54d0e0",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "8503f1f326475a132ba3",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.371095a0.chunk.css"
+  },
+  {
+    "revision": "8f0e721519d177a35b11",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0197419b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0197419b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8503f1f326475a132ba3",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.bcb2fdbf.chunk.js"
+  },
+  {
+    "revision": "c8d5cf6281a25dc7936b",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3548cc6e.js"
+  }
+]);
